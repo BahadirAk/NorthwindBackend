636f81de187b348c6a6fc389c5d35ae41c893911
BahadirAk <ak_bahad@hotmail.com>
2022-10-17 17:42:57 +0300

    Add project files.

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün başarıyla eklendi!!!";
+        public static string ProductUpdated = "Ürün başarıyla güncellendi!!!";
+        public static string ProductDeleted = "Ürün başarıyla silindi!!!";
+
+        public static string CategoryAdded = "Kategori başarıyla eklendi!!!";
+        public static string CategoryUpdated = "Kategori başarıyla güncellendi!!!";
+        public static string CategoryDeleted = "Kategori başarıyla silindi!!!";
+    }
+}
